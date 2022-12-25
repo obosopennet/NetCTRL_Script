b89a20c52a19e2b3a620404e2de19c3f5f04bf3e
Tom Sverre <68464234+HumlePung@users.noreply.github.com>
2022-12-25 20:54:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,72 @@
+import argparse
+import yaml
 
+# Import other files
+import jinja2_template
+import netmiko_netctrl
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--yaml", help="YAML file with configuration data")
+    parser.add_argument("-i", "--ip", help="IP address of device")
+    parser.add_argument("-d", "--device-type", help="Device type (e.g. cisco_ios)")
+    parser.add_argument("-s", "--send", action="store_true", help="Send configuration to devices")
+    parser.add_argument("-t", "--test", action="store_true", help="Test that the script is working without actually sending the configuration to devices")
+    args = parser.parse_args()
+
+    # Load configuration data from YAML file
+    with open(args.yaml, "r") as f:
+        config = yaml.safe_load(f)
+
+    # Retrieve device information from Netbox
+    if args.ip:
+        # Get device information for specific IP
+        device = netbox.dcim.devices.get(ip=args.ip)
+    elif args.device_type:
+        # Get device information for specific device type
+        devices = netbox.dcim.devices.filter(device_type=args.device_type)
+        if not devices:
+            print("No devices found with device type {}".format(args.device_type))
+            return
+        device = devices[0]
+    else:
+        # Get device information for all devices
+        devices = netbox.dcim.devices.all()
+        if not devices:
+            print("No devices found")
+            return
+          # Generate configuration
+if device.device_type.slug == "cisco_ios":
+    # Generate Cisco IOS configuration
+    template = "cisco_ios_template.j2"
+    context = {"hostname": device.name, "mgmt_ip": device.primary_ip4.address}
+    config_str = jinja2_template.render_template(template, context)
+elif device.device_type.slug == "cisco_xr":
+    # Generate Cisco XR configuration
+    template = "cisco_xr_template.j2"
+    context = {"hostname": device.name, "mgmt_ip": device.primary_ip4.address}
+    config_str = jinja2_template.render_template(template, context)
+else:
+    print("Unsupported device type: {}".format(device.device_type.slug))
+    return
+  # Send configuration to devices
+if args.send:
+    if args.test:
+        # Test that the script is working without actually sending the configuration to devices
+        print("Simulating sending configuration to {} ({})".format(device.name, device.primary_ip4.address))
+    else:
+        # Send configuration to devices
+        print("Sending configuration to {} ({})".format(device.name, device.primary_ip4.address))
+        netmiko_netctrl.send_config(device.primary_ip4.address, config_str, config["username"], config["password"])
+else:
+    print("Configuration:\n{}".format(config_str))
+    # Check status of devices
+if args.send:
+    # Check status of devices
+    print("Checking status of devices")
+    for device in devices:
+        status = netmiko_netctrl.check_status(device.primary_ip4.address, config["username"], config["password"])
+        print("{} ({}): {}".format(device.name, device.primary_ip4.address, status))
+
+print("Done!")
