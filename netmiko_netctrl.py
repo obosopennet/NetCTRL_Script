16cd2f9addeab408bf669d1f501b9b88b6482267
Tom Sverre <68464234+HumlePung@users.noreply.github.com>
2023-01-02 15:13:38 +0100

    Update netmiko_netctrl.py

--- netmiko_netctrl.py
+++ netmiko_netctrl.py
@@ -1,1 +1,39 @@
+from netmiko import ConnectHandler
+
+def send_config(device_ip, config_str, username, password):
+    """
+    Send configuration to a device using Netmiko.
+    """
+    device = {
+        "device_type": "cisco_ios",
+        "ip": device_ip,
+        "username": username,
+        "password": password
+    }
+
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_config_set(config_str)
+    net_connect.disconnect()
+
+    return output
+
+def check_status(device_ip, username, password):
+    """
+    Check the status of a device using Netmiko.
+    """
+    device = {
+        "device_type": "cisco_ios",
+        "ip": device_ip,
+        "username": username,
+        "password": password
+    }
+
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_command("show version")
+    net_connect.disconnect()
+
+    if "Cisco IOS Software" in output:
+        return "OK"
+    else:
+        return "Error"
 
